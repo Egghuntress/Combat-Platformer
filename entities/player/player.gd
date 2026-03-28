bb5edc1c543be2321a5dec583f65bec20ac94ef1
Egghuntress <73998797+Egghuntress@users.noreply.github.com>
2026-03-28 10:59:17 +0100

    feat(player): add player

---
+++ entities/player/player.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("left_button"):
+		#move to left
+		pass
+	if Input.is_action_pressed("right_button"):
+		#move to right
+		pass
